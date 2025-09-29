311bde8666cf35a426edee4761fbb7289dec340b
ShumCr <152937283+ShumCr@users.noreply.github.com>
2025-09-29 12:26:53 +0200

    Update EnumerableRoles.sol

--- src/auth/EnumerableRoles.sol
+++ src/auth/EnumerableRoles.sol
@@ -24,7 +24,7 @@ pragma solidity ^0.8.4;
 /// This implementation is compatible with any Ownable.
 /// This implementation is NOT compatible with OwnableRoles.
 
-// 1 23
+// 1 234
 abstract contract EnumerableRoles {
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                           EVENTS                           */
