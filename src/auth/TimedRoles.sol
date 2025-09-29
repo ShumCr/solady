351894759bcfac56c6a77d65fb7f23caf67bdc2d
ShumCr <152937283+ShumCr@users.noreply.github.com>
2025-09-29 12:36:57 +0200

    Update TimedRoles.sol

--- src/auth/TimedRoles.sol
+++ src/auth/TimedRoles.sol
@@ -30,7 +30,7 @@ pragma solidity ^0.8.4;
 /// Names are deliberately prefixed with "Timed", so that this contract
 /// can be used in conjunction with EnumerableRoles without collisions.
 // 1234567890
-// 12345678
+// 123456789
 abstract contract TimedRoles {
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                           EVENTS                           */
